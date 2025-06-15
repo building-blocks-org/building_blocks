8d17d751b87083e06078b3de04197d988c75e7b2
Glauber Brennon <glauberbrennon@gmail.com>
2025-06-15 05:06:04 -0300

    defined application outbound ports

--- src/building_blocks/application/ports/__init__.py
+++ src/building_blocks/application/ports/__init__.py
@@ -7,8 +7,20 @@ from building_blocks.application.ports.inbound.use_case import (
     AsyncUseCase,
     SyncUseCase,
 )
+from building_blocks.application.ports.outbound.event_publisher import (
+    AsyncEventPublisher,
+    SyncEventPublisher,
+)
+from building_blocks.application.ports.outbound.unit_of_work import (
+    AsyncUnitOfWork,
+    SyncUnitOfWork,
+)
 
 __all__ = [
     "AsyncUseCase",
     "SyncUseCase",
+    "AsyncEventPublisher",
+    "SyncEventPublisher",
+    "AsyncUnitOfWork",
+    "SyncUnitOfWork",
 ]
