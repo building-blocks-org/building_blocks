fb1e1de9816643ba45e9d0e245488faa4ac1d9b6
Glauber Brennon <glauberbrennon@gmail.com>
2025-06-15 10:49:50 -0300

    added DomainException

---
+++ src/building_blocks/domain/domain_exception.py
@@ -0,0 +1,9 @@
+class DomainException(Exception):
+    """Base class for all domain-related exceptions."""
+
+    """
+    This exception should be used to indicate issues that occur within the
+    domain layer, such as validation failures, business rule violations, or
+    other domain-specificissues.
+    """
+    pass
