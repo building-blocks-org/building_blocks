4087b00bd483a3ba30ccbbabda1ce722c70d2b74
Glauber Brennon <glauberbrennon@gmail.com>
2025-07-21 07:04:50 -0300

    added RegisterUserUseCase

--- examples/tasker_primitives/src/application/ports/__init__.py
+++ examples/tasker_primitives/src/application/ports/__init__.py
@@ -3,5 +3,17 @@ from .inbound.create_task_use_case import (
     CreateTaskResponse,
     CreateTaskUseCase,
 )
+from .inbound.register_user_use_case import (
+    RegisterUserRequest,
+    RegisterUserResponse,
+    RegisterUserUseCase,
+)
 
-__all__ = ["CreateTaskUseCase", "CreateTaskRequest", "CreateTaskResponse"]
+__all__ = [
+    "CreateTaskUseCase",
+    "CreateTaskRequest",
+    "CreateTaskResponse",
+    "RegisterUserUseCase",
+    "RegisterUserRequest",
+    "RegisterUserResponse",
+]
