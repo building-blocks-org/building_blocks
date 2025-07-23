eb6e200c691842a543aae69f2e5feb1e046211ca
Glauber Brennon <glauberbrennon@gmail.com>
2025-07-23 03:31:25 -0300

    infrastructur user persistence done

--- examples/tasker_primitives/src/infrastructure/persistence/models/__init__.py
+++ examples/tasker_primitives/src/infrastructure/persistence/models/__init__.py
@@ -1,8 +1,10 @@
 from .base import MappedModel, TimestampedBase
 from .task_model import TaskModel
+from .user_model import UserModel
 
 __all__ = [
     "MappedModel",
     "TimestampedBase",
     "TaskModel",
+    "UserModel",
 ]
--- examples/tasker_primitives/src/infrastructure/persistence/repositories/__init__.py
+++ examples/tasker_primitives/src/infrastructure/persistence/repositories/__init__.py
@@ -1,5 +1,7 @@
 from .sqlalchemy_task_repository import SQLAlchemyTaskRepository
+from .sqlalchemy_user_repository import SQLAlchemyUserRepository
 
 __all__ = [
     "SQLAlchemyTaskRepository",
+    "SQLAlchemyUserRepository",
 ]
