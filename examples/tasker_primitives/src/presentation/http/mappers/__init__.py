2804efa794b8d7e919700a9374a69b6b20462405
Glauber Brennon <glauberbrennon@gmail.com>
2025-07-23 03:37:49 -0300

    implemented RegisterUserDtoMapper

--- examples/tasker_primitives/src/presentation/http/mappers/__init__.py
+++ examples/tasker_primitives/src/presentation/http/mappers/__init__.py
@@ -1,3 +1,7 @@
 from .create_task_dto_mapper import CreateTaskDtoMapper
+from .register_user_dto_mapper import RegisterUserDtoMapper
 
-__all__ = ["CreateTaskDtoMapper"]
+__all__ = [
+    "CreateTaskDtoMapper",
+    "RegisterUserDtoMapper",
+]
