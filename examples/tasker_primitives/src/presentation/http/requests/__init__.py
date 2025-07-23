7db853bb4fa44c392e932eab14fa35fa5db46c88
Glauber Brennon <glauberbrennon@gmail.com>
2025-07-23 03:35:33 -0300

    adding RegisterUser request/response

--- examples/tasker_primitives/src/presentation/http/requests/__init__.py
+++ examples/tasker_primitives/src/presentation/http/requests/__init__.py
@@ -1,5 +1,7 @@
 from .create_task_http_request import CreateTaskHttpRequest
+from .register_user_http_request import RegisterUserHttpRequest
 
 __all__ = [
     "CreateTaskHttpRequest",
+    "RegisterUserHttpRequest",
 ]
--- examples/tasker_primitives/src/presentation/http/responses/__init__.py
+++ examples/tasker_primitives/src/presentation/http/responses/__init__.py
@@ -1,5 +1,7 @@
 from .create_task_http_response import CreateTaskHttpResponse
+from .register_user_http_response import RegisterUserHttpResponse
 
 __all__ = [
     "CreateTaskHttpResponse",
+    "RegisterUserHttpResponse",
 ]
