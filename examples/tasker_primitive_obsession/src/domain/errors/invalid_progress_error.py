cb3e6cfcb8977187f390e4e39b934f9906298756
Glauber Brennon <glauberbrennon@gmail.com>
2025-07-29 01:29:04 -0300

    fix on InvalidProgressError docstring

--- examples/tasker_primitive_obsession/src/domain/errors/invalid_progress_error.py
+++ examples/tasker_primitive_obsession/src/domain/errors/invalid_progress_error.py
@@ -2,7 +2,7 @@ from building_blocks.domain.domain_error import DomainError
 
 
 class InvalidProgressError(DomainError):
-    """Exception raised for errors in the input email format."""
+    """Exception raised for errors related to invalid  progress values."""
 
     def __init__(self, message: str = "Invalid progress value."):
         super().__init__(message)
