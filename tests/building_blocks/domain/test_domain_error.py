e728daf8e07bcdcd2339444eb9ea22b1939a017f
Glauber Brennon <glauberbrennon@gmail.com>
2025-07-29 01:38:07 -0300

    fix on test_domain_error.py

--- tests/building_blocks/domain/test_domain_error.py
+++ tests/building_blocks/domain/test_domain_error.py
@@ -6,4 +6,4 @@ from building_blocks.domain.domain_error import DomainError
 class TestDomainError:
     def test_domain_error_when_raised_is_an_exception(self):
         with pytest.raises(DomainError):
-            raise DomainError("This is a domain exception")
+            raise DomainError("This is a domain error")
