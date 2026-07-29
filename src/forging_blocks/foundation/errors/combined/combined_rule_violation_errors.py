939eb7cd353add7a6f9ea6f473df1dacb805c4cf
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:36:36 -0300

    feat(errors): add RuntimeErrorMixin to CombinedRuleViolationErrors

--- src/forging_blocks/foundation/errors/combined/combined_rule_violation_errors.py
+++ src/forging_blocks/foundation/errors/combined/combined_rule_violation_errors.py
@@ -5,8 +5,9 @@ RuleViolationError instances into a single error.
 """
 
 from forging_blocks.foundation.errors.base.rule_violation_error import RuleViolationError
+from forging_blocks.foundation.errors.builtin.runtime_error_mixin import RuntimeErrorMixin
 from forging_blocks.foundation.errors.combined.combined_errors import CombinedErrors
 
 
-class CombinedRuleViolationErrors(CombinedErrors[RuleViolationError]):
+class CombinedRuleViolationErrors(RuntimeErrorMixin, CombinedErrors[RuleViolationError]):
     """Aggregates multiple rule violation errors for easier handling and reporting."""
