0b3b59e5240db442618b1457adfaa800d58f4b34
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:37:00 -0300

    feat(errors): add RuntimeErrorMixin to EventStoreError

--- src/forging_blocks/application/errors/event_store_error.py
+++ src/forging_blocks/application/errors/event_store_error.py
@@ -1,10 +1,11 @@
 """Event store error type for application-level operations."""
 
 from forging_blocks.foundation.errors.base.error import Error
+from forging_blocks.foundation.errors.builtin.runtime_error_mixin import RuntimeErrorMixin
 from forging_blocks.foundation.errors.core import ErrorMessage
 
 
-class EventStoreError[MetadataValueType = object](Error[MetadataValueType]):
+class EventStoreError[MetadataValueType = object](RuntimeErrorMixin, Error[MetadataValueType]):
     """Base error for event store operations."""
 
     def __init__(self, message: str) -> None:
