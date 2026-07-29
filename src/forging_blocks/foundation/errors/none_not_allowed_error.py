dd8bfa784c1dc09fd3e01a2fca0254e5fcec1210
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:36:41 -0300

    feat(errors): add ValueErrorMixin to NoneNotAllowedError

--- src/forging_blocks/foundation/errors/none_not_allowed_error.py
+++ src/forging_blocks/foundation/errors/none_not_allowed_error.py
@@ -1,7 +1,8 @@
 """Error indicating that a None value was provided where it is not allowed."""
 
 from forging_blocks.foundation.errors.base.error import Error
+from forging_blocks.foundation.errors.builtin.value_error_mixin import ValueErrorMixin
 
 
-class NoneNotAllowedError(Error[str]):
+class NoneNotAllowedError(ValueErrorMixin, Error[str]):
     """Error indicating that a None value was provided where it is not allowed."""
