4abbd7882e4408ce6f9c890d5cc81832793c81a4
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:36:36 -0300

    feat(errors): add ValueErrorMixin to CombinedValidationErrors

--- src/forging_blocks/foundation/errors/combined/combined_validation_errors.py
+++ src/forging_blocks/foundation/errors/combined/combined_validation_errors.py
@@ -4,9 +4,10 @@ Defines CombinedValidationErrors which aggregates multiple
 ValidationFieldErrors instances into a single error.
 """
 
+from forging_blocks.foundation.errors.builtin.value_error_mixin import ValueErrorMixin
 from forging_blocks.foundation.errors.combined.combined_errors import CombinedErrors
 from forging_blocks.foundation.errors.combined.validation_field_errors import ValidationFieldErrors
 
 
-class CombinedValidationErrors(CombinedErrors[ValidationFieldErrors]):
+class CombinedValidationErrors(ValueErrorMixin, CombinedErrors[ValidationFieldErrors]):
     """Aggregates multiple validation errors for easier handling and reporting."""
