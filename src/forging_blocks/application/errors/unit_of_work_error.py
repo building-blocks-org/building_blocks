9e5edd110cbb3dc74c10554ee47a4fd1c2b1ec55
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:37:01 -0300

    feat(errors): add RuntimeErrorMixin to UnitOfWorkError

--- src/forging_blocks/application/errors/unit_of_work_error.py
+++ src/forging_blocks/application/errors/unit_of_work_error.py
@@ -1,5 +1,6 @@
 from forging_blocks.foundation.errors.base.error import Error
+from forging_blocks.foundation.errors.builtin.runtime_error_mixin import RuntimeErrorMixin
 
 
-class UnitOfWorkError[MetadataValueType = object](Error[MetadataValueType]):
+class UnitOfWorkError[MetadataValueType = object](RuntimeErrorMixin, Error[MetadataValueType]):
     """Error raised when a Unit of Work operation fails."""
