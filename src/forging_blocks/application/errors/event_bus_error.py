b78627ce57bf3e15fa4e4b317785ac12f5bc95ec
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:37:00 -0300

    feat(errors): add RuntimeErrorMixin to EventBusError

--- src/forging_blocks/application/errors/event_bus_error.py
+++ src/forging_blocks/application/errors/event_bus_error.py
@@ -1,10 +1,11 @@
 """Event bus error type for application-level operations."""
 
 from forging_blocks.foundation.errors.base.error import Error
+from forging_blocks.foundation.errors.builtin.runtime_error_mixin import RuntimeErrorMixin
 from forging_blocks.foundation.errors.core import ErrorMessage
 
 
-class EventBusError[MetadataValueType = object](Error[MetadataValueType]):
+class EventBusError[MetadataValueType = object](RuntimeErrorMixin, Error[MetadataValueType]):
     """Base error for event bus operations."""
 
     def __init__(self, message: str) -> None:
