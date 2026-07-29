7d1cc43ab3820dd45110b3c6e977d6056f03c89e
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-29 01:37:01 -0300

    feat(errors): add RuntimeErrorMixin to TransactionError

--- src/forging_blocks/application/errors/transaction_error.py
+++ src/forging_blocks/application/errors/transaction_error.py
@@ -1,7 +1,8 @@
 """Error raised when a transaction operation fails."""
 
 from forging_blocks.foundation.errors.base.error import Error
+from forging_blocks.foundation.errors.builtin.runtime_error_mixin import RuntimeErrorMixin
 
 
-class TransactionError[MetadataValueType = object](Error[MetadataValueType]):
+class TransactionError[MetadataValueType = object](RuntimeErrorMixin, Error[MetadataValueType]):
     """Error raised when a transaction operation fails."""
